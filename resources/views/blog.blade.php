73af9f801d7be3a4dd99db230e41b80842abea19
naveensajan <naveensajan@gmail.com>
2021-05-31 00:47:39 +0530

    travelapp

---
+++ resources/views/blog.blade.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>index</title>
+</head>
+<body background="pics/slide1.jpg">
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#"></a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link " aria-current="page" href="/">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="offers">Offers</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active" href="blog">blog</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="signup">Sign Up</a>
+              </li>
+            </ul>
+            <form class="d-flex">
+              <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+              <button class="btn btn-outline-dark" type="submit">Search</button>
+            </form>
+          </div>
+        </div>
+      </nav>
+      <div class="container">
+        <div class="row">
+            <div class="col col-12 col-sm-4 col-md-4 col-lg-4 col-md-4 col-xl-4 col-xxl-4"></div>
+            <div class="col col-12 col-sm-4 col-md-4 col-lg-4 col-md-4 col-xl-4 col-xxl-4">
+                <table>
+                <tr></tr>
+                <tr><br><br> <td><img src="pics/logo.png"  width="450" height="230" ></td></tr></table>
+               
+            </div>
+            <div class="col col-12 col-sm-4 col-md-4 col-lg-4 col-md-4 col-xl-4 col-xxl-4"></div>
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="row">
+            <div class="col col-12 col-sm-4 col-md-4 col-lg-4 col-md-4 col-xl-4 col-xxl-4">
+                <br><br><br>
+                <button type="button" class="btn btn-danger">15 FEB</button>
+                <div class="card text-center">
+                    <div class="card-header">
+                        Posted by Admin
+                    </div>
+                    <div class="card-body">
+                      <h5 class="card-title">BARCELONA</h5>
+                      <p class="card-text">Barcelona is a city on the coast of northeastern Spain. It is the capital and largest city of the autonomous community of Catalonia, as well as the second most populous municipality of Spain. With a population of 1.6 million within city limits,its urban area extends to numerous neighbouring municipalities within the Province of Barcelona and is home to around 4.8 million people,making it the fifth most populous urban area in the European Union after Paris, the Ruhr area, Madrid, and Milan.It is one of the largest metropolises on the Mediterranean Sea, located on the coast between the mouths of the rivers Llobregat and Besòs, and bounded to the west by the Serra de Collserola mountain range, the tallest peak of which is 512 metres (1,680 feet) high.</p>
+                      <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                    <div class="card-footer text-muted">
+                      2 days ago
+                    </div>
+                  </div>
+            </div>
+            <div class="col col-12 col-sm-4 col-md-4 col-lg-4 col-md-4 col-xl-4 col-xxl-4">
+                <br><br><br>
+                <button type="button" class="btn btn-danger">21 DEC</button>
+                <div class="card text-center">
+                    <div class="card-header">
+                        Posted by Admin
+                    </div>
+                    <div class="card-body">
+                      <h5 class="card-title">GOA</h5>
+                      <p class="card-text">Goa is a state on the southwestern coast of India within the region known as the Konkan, and geographically separated from the Deccan highlands by the Western Ghats.It is surrounded by the Indian states of Maharashtra to the north and Karnataka to the east and south, with the Arabian Sea forming its western coast. It is India's smallest state by area and its fourth-smallest by population. Goa has the highest GDP per capita among all Indian states,two and a half times as high as the GDP per capita of the country as a whole.The Eleventh Finance Commission of India named Goa the best-placed state because of its infrastructure, and India’s National Commission on Population rated it as having the best quality of life in India</p>
+                      <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                    <div class="card-footer text-muted">
+                      6 days ago
+                    </div>
+                  </div>
+            </div>
+            <div class="col col-12 col-sm-4 col-md-4 col-lg-4 col-md-4 col-xl-4 col-xxl-4">
+                <br><br><br>
+                <button type="button" class="btn btn-danger">22 JUN</button>
+                <div class="card text-center">
+                    <div class="card-header">
+                        Posted by Admin
+                    </div>
+                    <div class="card-body">
+                      <h5 class="card-title">PARIS</h5>
+                      <p class="card-text">Paris is the capital and most populous city of France, with an estimated population of 2,175,601 residents as of 2018, in an area of more than 105 square kilometres (41 square miles).The City of Paris is the centre and seat of government of the Île-de-France, or Paris Region, which has an estimated official 2017 population of 12,174,880, or about 18 percent of the population of France.According to the Economist Intelligence Unit Worldwide Cost of Living Survey in 2018, Paris was the second most expensive city in the world, after Singapore, and ahead of Zürich, Hong Kong, Oslo and Geneva.Another source ranked Paris as most expensive, on a par with Singapore and Hong Kong, in 2018.</p>
+                      <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                    <div class="card-footer text-muted">
+                      4 days ago
+                    </div>
+                  </div>
+            </div>
+        </div>
+    </div>
+ <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>   
+</body>
+</html>
---
+++ resources/views/index.blade.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>index</title>
+</head>
+<body background="pics/slide1.jpg">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#"></a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="/">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="offers">Offers</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="blog">blog</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="signup">Sign Up</a>
+              </li>
+            </ul>
+            <form class="d-flex">
+              <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+              <button class="btn btn-outline-dark" type="submit">Search</button>
+            </form>
+          </div>
+        </div>
+      </nav>
+      <div class="container">
+        <div class="row">
+            <div class="col col-12 col-sm-4 col-md-4 col-lg-4 col-md-4 col-xl-4 col-xxl-4"></div>
+            <div class="col col-12 col-sm-4 col-md-4 col-lg-4 col-md-4 col-xl-4 col-xxl-4">
+                <table>
+                <tr></tr>
+                <tr><br><br> <td><img src="pics/logo.png"  width="450" height="230" ></td></tr></table>
+               
+            </div>
+            <div class="col col-12 col-sm-4 col-md-4 col-lg-4 col-md-4 col-xl-4 col-xxl-4"></div>
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="row">
+            <div class="col col-12 col-sm-4 col-md-4 col-lg-4 col-md-4 col-xl-4 col-xxl-4">
+                <br><br><br>
+                <div class="card" style="width: 18rem;">
+                    <img src="pics/page2_img7.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                      <h5 class="card-title">BARCELONA</h5>
+                      <p class="card-text">
+                        FROM
+                        $ 1000</p>
+                      <a href="#" class="btn btn-primary">Learn More</a>
+                    </div>
+                  </div>
+            </div>
+            <div class="col col-12 col-sm-4 col-md-4 col-lg-4 col-md-4 col-xl-4 col-xxl-4">
+                <br><br><br>
+                <div class="card" style="width: 18rem;">
+                    <img src="pics/page2_img2.jpg " width="450" class="card-img-top" alt="...">
+                    <div class="card-body">
+                      <h5 class="card-title">GOA</h5>
+                      <p class="card-text">
+                        FROM $ 1.500</p>
+                      <a href="#" class="btn btn-primary">Learn More</a>
+                    </div>
+                  </div>
+            </div>
+            <div class="col col-12 col-sm-4 col-md-4 col-lg-4 col-md-4 col-xl-4 col-xxl-4">
+                <br><br><br>
+                <div class="card" style="width: 18rem;">
+                    <img src="pics/page2_img3.jpg" width="450" class="card-img-top" alt="...">
+                    <div class="card-body">
+                      <h5 class="card-title"> PARIS</h5>
+                      <p class="card-text">
+                       
+FROM
+$ 1.600</p>
+                      <a href="#" class="btn btn-primary">Learn More</a>
+                    </div>
+                  </div>
+            </div>
+        </div>
+    </div>
+ <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>   
+</body>
+</html>
